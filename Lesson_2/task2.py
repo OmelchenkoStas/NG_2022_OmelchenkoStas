83eb3111006cc0508b7aa1e32a6d42dd8f0f0a71
Omelchenko Stanislav <98589561+OmelchenkoStas@users.noreply.github.com>
2022-11-11 21:49:50 +0300

    Update task2.py

--- Lesson_2/task2.py
+++ Lesson_2/task2.py
@@ -1,4 +1,4 @@
-list1 = (input().split(","))
+list1 = input().split(",")
 print ("List: ", list1)
 list_without_duplicates = list(set(list1))
 print ("list without duplicates: ", list_without_duplicates)
